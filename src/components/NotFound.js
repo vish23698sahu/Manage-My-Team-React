ad9274d4b5fa6ef24216d7f39744519e8413433b
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-02-07 15:32:26 +0530

    v - 2 Not Found and Nav page

---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <header className='container'>
+            <div className='row justify-content-center mt-3 mb-4'>
+                <div className='col-8'>
+                    <h5 className='text-danger' >404 - Page you are looking for could not be Found</h5>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default NotFound
